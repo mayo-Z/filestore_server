157fbc5c56f3c2cd7e56b7080d6886ab0a494340
uptocorrupt <1113051093@qq.com>
2021-07-27 02:09:25 +0800

    4-2

---
+++ handler/user.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	dblayer "filestore_server/db"
+	"filestore_server/util"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	pwdSalt = "*#890"
+)
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		data, err := ioutil.ReadFile("./static/view/signup.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+		//http.Redirect(w, r, "/static/view/signup.html", http.StatusFound)
+		return
+	}
+
+	fmt.Println("-------------------------------------------------------")
+	r.ParseForm()
+
+	username := r.Form.Get("username")
+	passwd := r.Form.Get("password")
+
+	if len(username) < 3 || len(passwd) < 5 {
+		w.Write([]byte("Invalid parameter"))
+		return
+	}
+
+	// 对密码进行加盐及取Sha1值加密
+	encPasswd := util.Sha1([]byte(passwd + pwdSalt))
+	// 将用户信息注册到用户表中
+	suc := dblayer.UserSignup(username, encPasswd)
+	if suc {
+		w.Write([]byte("SUCCESS"))
+	} else {
+		w.Write([]byte("FAILED"))
+	}
+}
+
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+
+}
